20c085000f761f108a4005e9541a3f77b6a522fd
Darx <mcrivabode@gmail.com>
2022-10-11 21:18:31 -0700

    add clarity comments, clean syntax and typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-var city=""; //stores searched city
+var city= ""; //stores searched city
 
-// query selectors
+//query selectors
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-history");
 var currentCity = $("#current-city");
 var currentTemperature = $("#temperature");
-var currentHumidty= $("#humidity");
-var currentWSpeed=$("#wind-speed");
-var currentUvindex= $("#uv-index");
-var sCity=[];
+var currentHumidty = $("#humidity");
+var currentWSpeed = $("#wind-speed");
+var currentUvindex = $("#uv-index");
+var sCity = [];
 
-var APIKey="6f0b8ef5066bd374b8e3cc7ecf08e140";
+var APIKey = "6f0b8ef5066bd374b8e3cc7ecf08e140"; //personal api key
 
 // searches the city to see if it exists in the entries from the storage
-function find(c){
+function find(c) {
     for (var i=0; i<sCity.length; i++){
         if(c.toUpperCase()===sCity[i]){
             return -1;
@@ -22,34 +22,35 @@ function find(c){
     }
     return 1;
 }
-//Set up the API key
 
-// Display the curent and future weather to the user after grabing the city form the input text box.
-function displayWeather(event){
+//displays current weather, and 5-day forecast
+function displayWeather(event) {
     event.preventDefault();
-    if(searchCity.val().trim()!==""){
-        city=searchCity.val().trim();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
         currentWeather(city);
     }
 }
-// Here we create the AJAX call
+
+//AJAX call
 function currentWeather(city){
-    // Here we build the URL so we can get a data from server side.
+    //decalre url with parameters, city query, and api key
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    
     $.ajax({
-        url:queryURL,
-        method:"GET",
+        url: queryURL,
+        method: "GET",
     }).then(function(response){
 
-        // parse the response to display the current weather including the City name. the Date and the weather icon. 
-        console.log(response);
-        //Dta object from server side Api for icon property.
-        var weathericon= response.weather[0].icon;
-        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        console.log(response);//logs response to console to check info was received correctly
+        
+        //api generates appropriate icon for weather
+        var weatherIcon= response.weather[0].icon;
+        var iconUrl="https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
         // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
         var date=new Date(response.dt*1000).toLocaleDateString();
         //parse the response for name of city and concanatig the date and icon.
-        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconUrl+">");
         // parse the response to display the current temperature.
         // Convert the temp to fahrenheit
 
@@ -110,13 +111,13 @@ function forecast(cityid){
         for (i=0;i<5;i++){
             var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
             var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
-            var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+            var iconUrl="https://openweathermap.org/img/wn/"+iconcode+".png";
             var tempK= response.list[((i+1)*8)-1].main.temp;
             var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
             var humidity= response.list[((i+1)*8)-1].main.humidity;
         
             $("#fDate"+i).html(date);
-            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fImg"+i).html("<img src="+iconUrl+">");
             $("#fTemp"+i).html(tempF+"&#8457");
             $("#fHumidity"+i).html(humidity+"%");
         }
